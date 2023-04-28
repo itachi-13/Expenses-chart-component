b0a28f9a6502b4e7b0a15e1a97882d79f1050f69
itachi-13 <amierib013@gmail.com>
2023-04-28 23:35:23 +0300

    update

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-let isScrollingHorizontally = false;
-let startTouchX = 0;
+// let isScrollingHorizontally = false;
+// let startTouchX = 0;
 
-window.addEventListener('touchstart', function(e) {
-  startTouchX = e.touches[0].pageX;
-});
+// window.addEventListener('touchstart', function(e) {
+//   startTouchX = e.touches[0].pageX;
+// });
 
-window.addEventListener('touchmove', function(e) {
-  const touchX = e.touches[0].pageX;
+// window.addEventListener('touchmove', function(e) {
+//   const touchX = e.touches[0].pageX;
+//   const body = document.querySelector('body');
+
+//   if (touchX > startTouchX) {
+//     isScrollingHorizontally = true;
+//     body.style.backgroundColor = 'blue';
+//   } else {
+//     isScrollingHorizontally = false;
+//     body.style.backgroundColor = '';
+//   }
+// });
+
+// window.addEventListener('touchend', function() {
+//   if (!isScrollingHorizontally) {
+//     const body = document.querySelector('body');
+//     body.style.backgroundColor = '';
+//   }
+// });
+
+
+
+
+window.addEventListener('touchmove', function() {
   const body = document.querySelector('body');
 
-  if (touchX > startTouchX) {
-    isScrollingHorizontally = true;
     body.style.backgroundColor = 'blue';
-  } else {
-    isScrollingHorizontally = false;
-    body.style.backgroundColor = '';
-  }
+  
 });
 
-window.addEventListener('touchend', function() {
-  if (!isScrollingHorizontally) {
-    const body = document.querySelector('body');
-    body.style.backgroundColor = '';
-  }
-});
