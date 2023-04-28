ab182c0efdd9b390fc03294047caa453f5d98f94
itachi-13 <amierib013@gmail.com>
2023-04-28 23:32:45 +0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+let isScrollingHorizontally = false;
+let startTouchX = 0;
+
+window.addEventListener('touchstart', function(e) {
+  startTouchX = e.touches[0].pageX;
+});
+
 window.addEventListener('touchmove', function(e) {
-    const touchX = e.touches[0].pageX;
+  const touchX = e.touches[0].pageX;
+  const body = document.querySelector('body');
+
+  if (touchX > startTouchX) {
+    isScrollingHorizontally = true;
+    body.style.backgroundColor = 'blue';
+  } else {
+    isScrollingHorizontally = false;
+    body.style.backgroundColor = '';
+  }
+});
+
+window.addEventListener('touchend', function() {
+  if (!isScrollingHorizontally) {
     const body = document.querySelector('body');
-    
-    if (touchX > 0) {
-      body.style.backgroundColor = 'blue'; // Change the background color to red
-    } else {
-      body.style.backgroundColor = ''; // Revert to the default background color
-    }
-  });
-  
+    body.style.backgroundColor = '';
+  }
+});
